77ea0209421ebba23f196e239e60b2234e9e8c29
cameron-bret <c.bret@whiskerhead.com>
2018-12-15 22:48:16 -0500

    fixed show hide feature

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-console.log('hello')
+$("#correctLetterP").hide()
 
-$('#al')
+$("#alphabetLetterP").on('click', function () {
+    $("#correctLetterP").show()
+})
+
+$("#alphabetLetterP").on('click', function () {
+    $("#correctLetterP").show()
+})
+
+$("#alphabetLetterP").click(function () {
+    console.log("hello");
+});
+console.log("hello")
+
+// $("#alphabetLetterP").click(function () {
+//     $("#correctLetterP").show()
+// })
