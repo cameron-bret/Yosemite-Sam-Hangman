8112ef34c5e1fe35a1f17301ea382febce2af9f4
cameron-bret <c.bret@whiskerhead.com>
2018-12-15 23:10:08 -0500

    show hide modified

--- app.js
+++ app.js
@@ -4,14 +4,41 @@ $("#alphabetLetterP").on('click', function () {
     $("#correctLetterP").show()
 })
 
-$("#alphabetLetterP").on('click', function () {
-    $("#correctLetterP").show()
+$("#correctLetterI").hide()
+
+$("#alphabetLetterI").on('click', function () {
+    $("#correctLetterI").show()
 })
 
-$("#alphabetLetterP").click(function () {
-    console.log("hello");
-});
-console.log("hello")
+$("#correctLetterS").hide()
+
+$("#alphabetLetterS").on('click', function () {
+    $("#correctLetterS").show()
+})
+
+$("#correctLetterT").hide()
+
+$("#alphabetLetterT").on('click', function () {
+    $("#correctLetterT").show()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $("#alphabetLetterP").click(function () {
+//     console.log("hello");
+// });
+// console.log("hello")
 
 // $("#alphabetLetterP").click(function () {
 //     $("#correctLetterP").show()
