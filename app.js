e304fd6b6d34bca343c1a51e42e83e2aef6931d6
cameron-bret <c.bret@whiskerhead.com>
2018-12-16 08:22:11 -0500

    added logic to correct letter choices for show hide

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ $("#alphabetLetterP").on('click', function () {
     $("#correctLetterP").show()
 })
 
+$("#alphabetLetterP").on('click', function () {
+    $("#alphabetLetterP").hide()
+})
+
 $("#correctLetterI").hide()
 
 $("#alphabetLetterI").on('click', function () {
     $("#correctLetterI").show()
+})
+
+$("#alphabetLetterI").on('click', function () {
+    $("#alphabetLetterI").hide()
 })
 
 $("#correctLetterS").hide()
@@ -16,11 +24,40 @@ $("#alphabetLetterS").on('click', function () {
     $("#correctLetterS").show()
 })
 
+$("#alphabetLetterS").on('click', function () {
+    $("#alphabetLetterS").hide()
+})
+
 $("#correctLetterT").hide()
 
 $("#alphabetLetterT").on('click', function () {
     $("#correctLetterT").show()
 })
+
+$("#alphabetLetterT").on('click', function () {
+    $("#alphabetLetterT").hide()
+})
+
+$("#correctLetterO").hide()
+
+$("#alphabetLetterO").on('click', function () {
+    $("#correctLetterO").show()
+})
+
+$("#alphabetLetterO").on('click', function () {
+    $("#alphabetLetterO").hide()
+})
+
+$("#correctLetterL").hide()
+
+$("#alphabetLetterL").on('click', function () {
+    $("#correctLetterL").show()
+})
+
+$("#alphabetLetterL").on('click', function () {
+    $("#alphabetLetterL").hide()
+})
+
 
 
 
