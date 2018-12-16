40d5c786b53149a847ab76f4f9fb9715e4e6eb84
cameron-bret <c.bret@whiskerhead.com>
2018-12-16 08:33:38 -0500

    added hide functionality to incorrect alphabet choices

--- app.js
+++ app.js
@@ -58,25 +58,82 @@ $("#alphabetLetterL").on('click', function () {
     $("#alphabetLetterL").hide()
 })
 
+$("#alphabetLetterA").on('click', function () {
+    $("#alphabetLetterA").hide()
+})
 
+$("#alphabetLetterB").on('click', function () {
+    $("#alphabetLetterB").hide()
+})
 
+$("#alphabetLetterC").on('click', function () {
+    $("#alphabetLetterC").hide()
+})
 
+$("#alphabetLetterD").on('click', function () {
+    $("#alphabetLetterD").hide()
+})
 
+$("#alphabetLetterE").on('click', function () {
+    $("#alphabetLetterE").hide()
+})
 
+$("#alphabetLetterF").on('click', function () {
+    $("#alphabetLetterF").hide()
+})
 
+$("#alphabetLetterG").on('click', function () {
+    $("#alphabetLetterG").hide()
+})
 
+$("#alphabetLetterH").on('click', function () {
+    $("#alphabetLetterH").hide()
+})
 
+$("#alphabetLetterJ").on('click', function () {
+    $("#alphabetLetterJ").hide()
+})
 
+$("#alphabetLetterK").on('click', function () {
+    $("#alphabetLetterK").hide()
+})
 
+$("#alphabetLetterM").on('click', function () {
+    $("#alphabetLetterM").hide()
+})
 
+$("#alphabetLetterN").on('click', function () {
+    $("#alphabetLetterN").hide()
+})
 
+$("#alphabetLetterQ").on('click', function () {
+    $("#alphabetLetterQ").hide()
+})
 
+$("#alphabetLetterR").on('click', function () {
+    $("#alphabetLetterR").hide()
+})
 
-// $("#alphabetLetterP").click(function () {
-//     console.log("hello");
-// });
-// console.log("hello")
+$("#alphabetLetterU").on('click', function () {
+    $("#alphabetLetterU").hide()
+})
 
-// $("#alphabetLetterP").click(function () {
-//     $("#correctLetterP").show()
-// })
+$("#alphabetLetterV").on('click', function () {
+    $("#alphabetLetterV").hide()
+})
+
+$("#alphabetLetterW").on('click', function () {
+    $("#alphabetLetterW").hide()
+})
+
+$("#alphabetLetterX").on('click', function () {
+    $("#alphabetLetterX").hide()
+})
+
+$("#alphabetLetterY").on('click', function () {
+    $("#alphabetLetterY").hide()
+})
+
+$("#alphabetLetterZ").on('click', function () {
+    $("#alphabetLetterZ").hide()
+})
